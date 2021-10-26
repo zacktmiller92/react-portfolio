425e6e560b7b2c75f7a89ca3fe40452c1d2699a7
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-26 16:17:11 -0400

    added contact page

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import Project from './components/Project';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
+import Contact from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
+
+  // const [pages] = useState([
+  //   { name: 'About', id:1},
+  //   { name: 'Projects', id:2},
+  //   { name: 'Contact', id:3},
+  //   { name: 'Resume', id:4}
+  // ]);
+
+  // const [currentPage, setCurrentPage] = useState(pages[0]);
+
+
   return (
     <main>
-      <Header>
-      </Header>
-      <About>
-        
-      </About>
-      <Project>
-        
-      </Project>
-
+      <Header></Header>
+      <About></About>
+      <Project></Project>
+      <Contact></Contact>
       <Footer></Footer>
     </main>
   );
---
+++ src/components/Contact/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+
+
+function Contact() {
+
+
+    return (
+        <Container>
+            <p>contact page</p>
+        </Container>
+    )
+}
+
+export default Contact;
