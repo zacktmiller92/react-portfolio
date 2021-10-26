b25bd28f28f88edf79948c851ebefc85bbd88023
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-26 16:19:07 -0400

    added resume page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
 import Contact from './components/Contact';
+import Resume from './components/Resume';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -27,6 +28,7 @@ function App() {
       <About></About>
       <Project></Project>
       <Contact></Contact>
+      <Resume></Resume>
       <Footer></Footer>
     </main>
   );
---
+++ src/components/Resume/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+
+
+function Resume() {
+
+
+    return (
+        <Container>
+            <p>resume page</p>
+        </Container>
+    )
+}
+
+export default Resume;
