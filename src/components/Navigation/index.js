f224829e30795a8ff62cb1e08245e802ef69d8c7
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-27 14:18:32 -0400

    updated className on navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -20,15 +20,15 @@ function Navigation(props) {
     return (
 
         <div className="container">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
                     <img className="nav-brand" src={logo}></img>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
                                 <a className="nav-link" href="#" onClick={() => setCurrentPage(pages[0])} >About</a>
                             </li>
                             <li class="nav-item">
