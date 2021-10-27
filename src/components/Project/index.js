437efff24f4586a496a0b797813f843c3145d0c0
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-27 17:04:20 -0400

    updated styling for projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -13,29 +13,29 @@ function Project() {
   return (
 
     <div className="container">
-      <div className="row">
-        <div className="col">
+      <div className="row m-4">
+        <div className="col-12 col-md-6 p-1">
           <a href="https://zacktmiller92.github.io/pokedex/" target="_blank" rel="noopener noreferrer">
-            <img src={background1} style={imgStyles}></img>
+            <img className="img-fluid" src={background1}></img>
           </a>
         </div>
 
-        <div className="col">
+        <div className="col-12 col-md-6 p-1">
           <a href="https://github.com/Chasetopher/deep-blue" target="_blank" rel="noopener noreferrer">
-            <img src={background2} style={imgStyles}></img>
+            <img className="img-fluid" src={background2}></img>
           </a>
         </div>
       </div>
 
-      <div className="row">
-        <div className="col">
+      <div className="row m-4">
+        <div className="col-12 col-md-6 p-1">
           <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-            <img src={background3} style={imgStyles}></img>
+            <img className="img-fluid" src={background3}></img>
           </a>
         </div>
 
-        <div className="col">
-          <img src={background3} style={imgStyles}></img>
+        <div className="col-12 col-md-6 p-1">
+          <img className="img-fluid" src={background3}></img>
         </div>
       </div>
     </div>
