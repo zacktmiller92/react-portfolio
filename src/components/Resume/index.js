1796bdd6e8b94ef0e2a54166e40cd65b28c5869f
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-27 16:17:25 -0400

    added resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,8 +7,15 @@ function Resume() {
 
     return (
         <div className="container">
-            <p>resume page</p>
+            <div className="row d-flex justify-content-center">
+                <div className="col col-lg-6 text-center">
+                    <p>View my resume and work experience on LikedIn</p>
+                    <a className="btn btn-danger" href="https://www.linkedin.com/in/zack-miller/" role="button">
+                        View LinkedIn
+                    </a>
+                </div>
             </div>
+        </div>
     )
 }
 
