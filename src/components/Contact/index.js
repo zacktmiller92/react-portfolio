0e82742ccda454583bd8ef2bf5da66d9b0364ce9
zacktmiller92 <zacktmiller92@gmail.com>
2021-10-27 16:03:29 -0400

    added contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,7 +7,29 @@ function Contact() {
 
     return (
         <div className="container">
-            <p>contact page</p>
+            <div className="row justify-content-md-center">
+                <div className="col col-lg-6">
+
+                    <form>
+
+                        <div class="mb-3">
+                            <label htmlFor="InputName1" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="InputName1" aria-describedby="nameHelp" />
+                        </div>
+
+                        <div class="mb-3">
+                            <label htmlFor="InputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" />
+                        </div>
+                        <div class="mb-3">
+                            <label htmlFor="InputMessage1" class="form-label">Message</label>
+                            <textarea type="text" class="form-control" id="InputMessage1" />
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+                </div>
+            </div>
         </div>
     )
 }
