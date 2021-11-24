c32811dcb9ba10fb362d57abd7f7c59e5f55b997
zacktmiller92 <zacktmiller92@gmail.com>
2021-11-23 20:29:34 -0500

    added project 3

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import background1 from '../../assets/img/project-1-screenshot.png'
 import background2 from '../../assets/img/project-2-screenshot.png'
-import background3 from '../../assets/img/milkyway.jpg'
+import background3 from '../../assets/img/project-3-screenshot.png'
+import background4 from '../../assets/img/milkyway.jpg'
 
 function Project() {
 
@@ -29,13 +30,13 @@ function Project() {
 
       <div className="row m-4">
         <div className="col-12 col-md-6 p-1">
-          <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://only-bans.herokuapp.com/" target="_blank" rel="noopener noreferrer">
             <img className="img-fluid" src={background3}></img>
           </a>
         </div>
 
         <div className="col-12 col-md-6 p-1">
-          <img className="img-fluid" src={background3}></img>
+          <img className="img-fluid" src={background4}></img>
         </div>
       </div>
     </div>
